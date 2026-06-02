751888e1144fbb5c49fa4d7a864e3942519067e5
Baellaaa <daniellavanmourik@gmail.com>
2026-06-02 15:55:41 +0200

    Add 3D models and enhance game functionality with new assets and game state management

--- js/dlc_gamescript.js
+++ js/dlc_gamescript.js
@@ -5,3 +5,44 @@ cylinder.setAttribute("height", "2");
 cylinder.setAttribute("radius", "0.75");
 cylinder.setAttribute("position", "3 1 0");
 scene.appendChild(cylinder);
+
+const gameState = {
+  creaturesDefeated: 0,
+
+  level1: {
+    berriesCollected: 0,
+    completed: false
+  },
+
+  level2: {
+    cakeComplete: false,
+    waterComplete: false,
+    completed: false
+  },
+
+  level3: {
+    bridgeCleared: false,
+    spinningWheelsDestroyed: 0,
+    completed: false
+  }
+};
+
+const sequence = ["a", "w", "d"];
+let playerInput = [];
+
+function pressButton(button){
+
+    playerInput.push(button);
+
+    const current =
+      playerInput.length - 1;
+
+    if(playerInput[current] !== sequence[current]){
+        failRound();
+        return;
+    }
+
+    if(playerInput.length === sequence.length){
+        nextRound();
+    }
+}
